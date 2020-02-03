8b65bd35138f3b2a1e4d765bf5c58f168671e4c9
Konstantin <it.openup@gmail.com>
2020-02-03 17:10:20 +0300

    Init;

---
+++ Lesson_Services/app/src/main/java/com/letsandroid/lesson_services/ListAdapter.java
@@ -0,0 +1,61 @@
+package com.letsandroid.lesson_services;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
+
+    private String[] items;
+    private OnItemClickListener onItemClickListener;
+
+    public void setItems(String[] items) {
+        this.items = items;
+    }
+
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
+        this.onItemClickListener = onItemClickListener;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
+
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+        holder.title.setText(items[position]);
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onItemClickListener.onClick(position);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return items == null ? 0 : items.length;
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView title;
+
+        ViewHolder(View view) {
+            super(view);
+            title = view.findViewById(R.id.item_title);
+        }
+    }
+
+    public interface OnItemClickListener {
+        void onClick(int position);
+    }
+}
+
---
+++ Lesson_Services/app/src/main/java/com/letsandroid/lesson_services/MainActivity.java
@@ -0,0 +1,47 @@
+package com.letsandroid.lesson_services;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final int START_FROM = 1970;
+    private final int COUNT = 100;
+
+    private String[] listItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListAdapter adapter = new ListAdapter();
+        adapter.setOnItemClickListener(new ListAdapter.OnItemClickListener() {
+            @Override
+            public void onClick(int position) {
+                Toast.makeText(MainActivity.this, listItems[position], Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        RecyclerView list = findViewById(R.id.list);
+        list.setAdapter(adapter);
+        list.setLayoutManager(new LinearLayoutManager(this));
+
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this, LinearLayout.VERTICAL);
+        dividerItemDecoration.setDrawable(getResources().getDrawable(R.drawable.list_item_divider, null));
+        list.addItemDecoration(dividerItemDecoration);
+
+        listItems = new String[COUNT];
+        for (int i = START_FROM; i < START_FROM + COUNT; i++) {
+            listItems[i - START_FROM] = String.valueOf(i);
+        }
+        adapter.setItems(listItems);
+        adapter.notifyDataSetChanged();
+    }
+}
